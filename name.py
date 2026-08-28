c696cba223f52b6c8bb0e7e6c9d67aa3c3aefd9a
NikitaS <nikitaniku2120@gmail.com>
2026-08-28 11:27:40 +0530

    Modify the file

--- name.py
+++ name.py
@@ -1,4 +1,6 @@
 name = input("enter your name")
 surname = input("Enter your surname")
+contact = input("Enter your contact Details")
 
 print("Your details:",name+" "+surname)
+print("Contact details is:",contact)
